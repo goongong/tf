6e0f538fa4bbf04fc648c097557838153b4e1061
goongong <chuhan_zhao@sina.com>
2018-01-18 00:40:43 -0800

    delete

--- lastedlong.py
+++
@@ -1,8 +0,0 @@
-import tensorflow as tf
-from tensorflow.python.framework import graph_util
-
-v1 = tf.Variable(tf.constant(1.0, shape=[1]), name="v1")
-v2 = tf.Variable(tf.constant(2.0, shape=[1]), name="v2")
-result = v1 + v2
-
-
